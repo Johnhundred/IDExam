ce07b945e3a9d9e7d5d4d686eb152a391003b750
Johannes Skjærbæk <john@particledesigns.dk>
2017-05-13 23:48:45 +0100

    admin page under dev

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -1,16 +1,31 @@
-
+// ctrl-f "placeholder" without the quotes to find things we need to fix before we're done
 
 jQuery("document").ready(function() {
     var $ = jQuery.noConflict();
     console.log("Page loaded and ready.");
 
-    // VARS
+    // VARS & SETUP
+    var jAdmins = {"admins":[]};
+    var bAdmin;
+
+    handleAdminSetup();
 
 
 
     // EVENTS
 
+    //Admin login form submit event
+    $("#lblAdminLogin").submit(function(e){
+        //prevent submitting of form & page reload, there is no backend to submit to
+        e.preventDefault();
+        //pass element (form) to handler function
+        adminHandleLogin(this);
+    });
 
+    //when an admin pick link is clicked, fade picker windows
+    $(".wdw-admin-picker ul li a").click(function(){
+        adminPicker(this);
+    });
 
     // FUNCTIONS - GENERAL
 
@@ -18,7 +33,87 @@ jQuery("document").ready(function() {
 
     // FUNCTIONS - ADMIN
 
+    // If no admins exist, create standard admin user, otherwise load admins from localStorage
+    function handleAdminSetup(){
+        if (localStorage.getItem("sAdmins") === null) {
+            var jAdmin = {"id": generateStringId(), "username":"admin", "password":"admin"};
+            jAdmins.admins.push(jAdmin);
+            localStorage.sAdmins = JSON.stringify(jAdmins);
+        } else {
+            jAdmins = JSON.parse(localStorage.sAdmins);
+            console.log(jAdmins);
+        }
+    }
 
+    function getAdmins(){
+        jAdmins = JSON.parse(localStorage.sAdmins);
+    }
+
+    function setAdmins(jAdmin){
+        jAdmins = JSON.parse(localStorage.sAdmins);
+        jAdmins.admins.push(jAdmin);
+        localStorage.sAdmins = JSON.stringify(jAdmins);
+        return true;
+    }
+
+    function checkAdmin(){
+        if(localStorage.getItem("bAdmin") === null){
+            localStorage.bAdmin = false;
+            bAdmin = false;
+        } else {
+            bAdmin = localStorage.bAdmin;
+        }
+
+        return bAdmin;
+    }
+
+    //handles admin login form submit - receives form element & children as oElement
+    function adminHandleLogin(oElement){
+        //gets input values from username & password fields
+        var sUsername = $(oElement).children("#lblAdminUsername").val();
+        var sPassword = $(oElement).children("#lblAdminPassword").val();
+
+        //check vs. registered users (also in localstorage) to login
+        getAdmins();
+        var iCounter = jAdmins.admins.length;
+        for(var i = 0; i < iCounter; i++){
+            if(jAdmins.admins[i].username == sUsername && jAdmins.admins[i].password == sPassword){
+                localStorage.bAdmin = true;
+                //placeholder, use animation instead?
+                console.log("Admin login successful.");
+                break;
+            } else {
+                localStorage.bAdmin = false;
+            }
+        }
+
+        $(oElement).children("#lblAdminUsername").val("");
+        $(oElement).children("#lblAdminPassword").val("");
+    }
+
+    function adminPicker(oElement){
+        //if(checkAdmin() == true){
+            $(".pick-window").css("display", "none");
+
+            //placeholder - implement css animation loader figure before showing each list
+            if($(oElement).hasClass("admin-event-list")){
+                $(".wdw-event-list").fadeIn();
+            } else if($(oElement).hasClass("admin-partner-list")){
+                $(".wdw-partner-list").fadeIn();
+            } else if($(oElement).hasClass("admin-admin-list")){
+                $(".wdw-admin-list").fadeIn();
+                console.log("admin");
+            }
+        //}
+    }
+
+    //generate (not really) random string to be used as an id
+    function generateStringId(){
+        var sResult = "";
+        sResult = (Math.random().toString(16)+"000000000").substr(2,8);
+        sResult = sResult + $.now().toString();
+        return sResult;
+    }
 
 });
 
