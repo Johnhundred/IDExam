ada9bab944c493ff10f42d7e0fe087227a8056ac
Johannes Skjærbæk <john@particledesigns.dk>
2017-05-13 21:22:52 +0100

    admin & JS setup

---
+++ app/assets/js/main.js
@@ -0,0 +1,24 @@
+
+
+jQuery("document").ready(function() {
+    var $ = jQuery.noConflict();
+    console.log("Page loaded and ready.");
+
+    // VARS
+
+
+
+    // EVENTS
+
+
+
+    // FUNCTIONS - ADMIN
+
+
+
+    // FUNCTIONS - GENERAL
+
+
+
+});
+
